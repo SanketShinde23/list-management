92323123324496ec1170d054186d9acf498a73a8
SANKET  SHINDE <148588953+SanketShinde23@users.noreply.github.com>
2025-02-09 12:34:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListManager from './components/ListManager';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+
+      <h1>LIST MANAGEMENT</h1>
+      <ListManager />
+    </div>
+  );
+}
+
+export default App;
